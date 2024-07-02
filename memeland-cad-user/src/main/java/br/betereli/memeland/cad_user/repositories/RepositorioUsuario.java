03b758a5d49e52a26f9ad5a4e4ed5d51b991d11a
Renan Betereli <renan@mantec.tech>
2024-07-02 18:59:06 -0300

    implemented tests for memeland-cad-user service

--- memeland-cad-user/src/main/java/br/betereli/memeland/cad_user/repositories/RepositorioUsuario.java
+++ memeland-cad-user/src/main/java/br/betereli/memeland/cad_user/repositories/RepositorioUsuario.java
@@ -2,6 +2,8 @@ package br.betereli.memeland.cad_user.repositories;
 
 import br.betereli.memeland.cad_user.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RepositorioUsuario extends JpaRepository<Usuario, Long> {
 }
--- memeland-cad-user/src/test/java/br/com/ebac/memelandia/MemelandiaApplicationTests.java
+++ memeland-cad-user/src/test/java/br/com/ebac/memelandia/MemelandiaApplicationTests.java
@@ -1,15 +1,86 @@
 package br.com.ebac.memelandia;
 
+import br.betereli.memeland.cad_user.MemelandiaApplication;
+import br.betereli.memeland.cad_user.entities.Usuario;
+import br.betereli.memeland.cad_user.repositories.RepositorioUsuario;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(classes = MemelandiaApplication.class)
 class MemelandiaApplicationTests {
 
+	@Autowired
+	private RepositorioUsuario repositorioUsuario;
+	@DisplayName("Save Then Return New User")
 	@Test
-	void contextLoads() {
+	void testSaveThenReturnNewUser() {
+		Usuario userTest = new Usuario();
+		userTest.setNome("Renan");
+		userTest.setEmail("renan@test.com");
+		userTest.setDataCadastro(new Date());
+
+		Usuario usuarioSalvo = repositorioUsuario.save(userTest);
+
+		assertNotNull(usuarioSalvo);
+		assertTrue(usuarioSalvo.getId() > 0);
 
 	}
+
+	@DisplayName("Save Then Return User List")
+	@Test
+	void testSaveThenReturnList() {
+		Usuario userTest = new Usuario();
+		userTest.setNome("Renan");
+		userTest.setEmail("renan@test.com");
+		userTest.setDataCadastro(new Date());
+		repositorioUsuario.save(userTest);
+
+		Usuario userTest1 = new Usuario();
+		userTest1.setNome("Tais");
+		userTest1.setEmail("tais@test.com");
+		userTest1.setDataCadastro(new Date());
+		repositorioUsuario.save(userTest1);
+
+		Usuario userTest2 = new Usuario();
+		userTest2.setNome("Luisa");
+		userTest2.setEmail("luisa@test.com");
+		userTest2.setDataCadastro(new Date());
+
+
+		repositorioUsuario.save(userTest2);
+
+		List<Usuario> usuarioList = repositorioUsuario.findAll();
+
+		assertNotNull(usuarioList);
+		assertEquals(3 , usuarioList.size());
+
+	}
+
+	@DisplayName("Save Then Return User by Id")
+	@Test
+	void testSaveThenReturnUserById() {
+		Usuario userTest = new Usuario();
+		userTest.setNome("Renan");
+		userTest.setEmail("renan@test.com");
+		userTest.setDataCadastro(new Date());
+
+		repositorioUsuario.save(userTest);
+
+		Usuario usuarioSalvo = repositorioUsuario.findById(userTest.getId()).get();
+
+		assertNotNull(usuarioSalvo);
+		assertEquals(userTest.getId() ,usuarioSalvo.getId());
+
+	}
+
 
 	/*
 
